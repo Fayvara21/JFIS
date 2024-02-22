bd9d48f715b13ad6cf53633c627f484ac28f9c6b
Fayvara21 <thelogane974@gmail.com>
2024-02-22 15:45:56 +0100

    l

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = 'root';
+$dbname = 'social_network';
+$port = 3306;
+
+//On essaie de se connecter
+try{
+    $pdo = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    //On définit le mode d'erreur de PDO sur Exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo 'Connexion réussie';
+}
+    /*On capture les exceptions si une exception est lancée et on affiche
+     *les informations relatives à celle-ci*/
+catch(PDOException $e){
+    echo "Erreur : " . $e->getMessage();
+}
+?>
