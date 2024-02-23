8643a07110f8fb1e4a6b8c0e7a6e37ef2c0c5992
Fayvara21 <thelogane974@gmail.com>
2024-02-23 11:32:04 +0100

    k

---
+++ php/post.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="../css/materialize.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="../css/post.css">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+  <div class="chatbox superblack">
+    <div> 
+        <form class="chat_input">
+          <a class="btn-floating grey"><i class="material-icons">add</i></a>
+          <input class="message" type="text" placeholder="Envoyer un message">
+          
+        </form>
+            
+          
+        
+ 
+    </div>
+  </div>
+</body>
+</html>
