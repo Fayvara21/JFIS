d2c4f14a5ed27431177febaad8bb97255be3236e
DeaZounours <theo1638@gmail.com>
2024-02-23 10:52:04 +0100

    c

--- README.md
+++ README.md
@@ -89,3 +89,30 @@ Check out the [CONTRIBUTING document](CONTRIBUTING.md) in the root of the reposi
 
 ## Copyright and license
 Code Copyright 2018 Materialize. Code released under the MIT license.
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'jefis';
+$port = 3306;
+
+//On essaie de se connecter
+try{
+    $pdo = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    //On définit le mode d'erreur de PDO sur Exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo 'Connexion réussie';
+}
+try {
+    $sql = " INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `profile_picture`, `email`, `password`, `birthday`, `phone`, `active`, `created_at`) VALUES (NULL, 'Moi2', 'Mwa', 'me', '', 'moi@mwa.fr', 'moi', '2024-02-16', '0707070707', '1', '2024-02-23 08:26:13.000000')";
+    $pdo->exec($sql);
+    echo "Nouveau compte créer";
+    } catch(PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+      }
+      $conn = null
+?>
+if(isset($_POST["nom"]) && isset($_POST["prenom"]))
+{ echo "<h2> Bonjour ". stripslashes($_POST["nom"]). "
+vous êtes ".stripslashes($_POST["prenom"]).
+" nouveau en PHP</h2>";
--- process_inscription.php
+++ process_inscription.php
@@ -1,6 +1,37 @@
 <?php
-if(isset($_POST["nom"]) && isset($_POST["prenom"]))
-{ echo "<h2> Bonjour ". stripslashes($_POST["nom"]). "
-vous êtes ".stripslashes($_POST["prenom"]).
-" nouveau en PHP</h2>";
-} ?>
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'jefis';
+$port = 3306;
+
+//On essaie de se connecter
+try{
+    $pdo = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    //On définit le mode d'erreur de PDO sur Exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo 'Connexion réussie';
+}
+    /*On capture les exceptions si une exception est lancée et on affiche
+     *les informations relatives à celle-ci*/
+catch(PDOException $e){
+    echo "Erreur : " . $e->getMessage();
+}    
+$prenom = $_POST['prenom'];
+$nom = $_POST['nom'];
+$pseudo = $_POST['pseudo'];
+$email = $_POST['email'];
+$mdp = $_POST['mot_de_passe'];
+$anniv = $_POST['anniversaire'];
+$tel = $_POST['telephone'];
+  
+try {
+    $sql = " INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `profile_picture`, `email`, `password`, `birthday`, `phone`, `active`) VALUES (NULL, '$prenom', '$nom', '$pseudo', '', '$email', '$mdp', '$anniv', '$tel', '1')";
+    $pdo->exec($sql);
+    echo "Nouveau compte créer";
+    } catch(PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+      }
+      $conn = null
+     
+?>
