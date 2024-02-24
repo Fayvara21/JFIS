ddc8ec102afcea4787382851064ff967ec5d75c7
Fayvara21 <thelogane974@gmail.com>
2024-02-24 09:42:35 +0100

    d

--- php/post.php
+++ php/post.php
@@ -5,7 +5,7 @@
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="../css/materialize.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
     <link rel="stylesheet" href="../css/post.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -14,18 +14,20 @@
 <body>
 
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  
+  <script src="../js/materialize.min.js"></script>
 
-  <ul class="right hide-on-med-and-down">
-    <li><a href="#!">First Sidebar Link</a></li>
-    <li><a href="#!">Second Sidebar Link</a></li>
-  </ul>
-  <ul id="slide-out" class="side-nav">
-    <li><a href="#!">First Sidebar Link</a></li>
-    <li><a href="#!">Second Sidebar Link</a></li>
-  </ul>
-  <a href="#" data-activates="slide-out" class="button-collapse" ><i class="mdi-navigation-menu"></i></a>
+  <ul id="slide-out" class="sidenav sidenav-fixed" >
+    
+    <li><a class="subheader">JFIS</a></li>
+    <li><a class="waves-effect" href="#!">Page d'acceuil</a></li>
+    <li><a class="waves-effect" href="#!">Ecrire un poste</a></li>
+    <li><a class="waves-effect" href="#!">Envoyer un message privé</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="waves-effect" href="#!">Informations du compte</a></li>
+    <li><a class="waves-effect" href="#!">Paramètres</a></li>
 
+  </ul>
+  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons white">menu</i></a>
 
 
 
