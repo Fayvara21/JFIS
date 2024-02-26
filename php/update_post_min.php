1e32a29b1f4fee45fe104ddcfd71759e58c33a95
Fayvara21 <thelogane974@gmail.com>
2024-02-26 09:07:15 +0100

    k

--- php/update_post_min.php
+++ php/update_post_min.php
@@ -5,7 +5,7 @@
     $database = "jefis";  
     $port = 3306;
 
-
+ 
     $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = $conn->prepare("SELECT * FROM messages");
