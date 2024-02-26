9e7197ddea6814b270649ce4d109c5c43ac990cc
Theo Boyet <theo1638@gmail.com>
2024-02-26 16:23:21 +0100

    t

---
+++ log.php
@@ -0,0 +1,27 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'jefis';
+$port = 3306;
+
+//On essaie de se connecter
+try{
+    $pdo = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    //On définit le mode d'erreur de PDO sur Exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo 'Connexion réussie';
+}
+    /*On capture les exceptions si une exception est lancée et on affiche
+     *les informations relatives à celle-ci*/
+catch(PDOException $e){
+    echo "Erreur : " . $e->getMessage();
+} 
+$mail = $_POST['email'];
+$mail = $_POST['prenom'];
+
+session_start();
+if(!$_SESSION['id']){
+      header('Location: /login.php');
+}
+else 
