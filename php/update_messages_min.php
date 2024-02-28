a941c554ff4f50bbeb418ae99feb78df76227563
Fayvara21 <thelogane974@gmail.com>
2024-02-28 10:12:36 +0100

    l

--- php/update_messages_min.php
+++ php/update_messages_min.php
@@ -8,7 +8,7 @@
  
     $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = $conn->prepare("SELECT * FROM messages ");
+    $sql = $conn->prepare("SELECT * FROM messages");
     $sql->execute();
     $result = $sql->fetchAll(PDO::FETCH_ASSOC);
     header('Content-Type: application/json');
