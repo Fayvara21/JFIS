de387f187e09ed9708e0af23311b2cbaa50dfa1e
Fayvara21 <thelogane974@gmail.com>
2024-02-29 14:48:29 +0100

    date

--- php/profil.php
+++ php/profil.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
-    <link rel="stylesheet" href="../css/accueil.css"> 
+    <link rel="stylesheet" href="../css/messages.css"> 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta charset="UTF-8">
